9e9e6c35f1288a60eb53a7c0ab1d74ce9ad3207c
ytbeom <ytbeom@gmail.com>
2014-03-15 01:09:39 +0900

    first commit

---
+++ OpenGLintro/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <gl/freeglut.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+using namespace std;
+void renderScene(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.5,0.5,0.0f);
+	glVertex3f(0.5,0.0,0.0);
+	glVertex3f(0.0,0.5,0.0);
+	glEnd();
+	glFlush();
+}
+void main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(320,320);
+	glutCreateWindow("Hello OpenGL");
+	glutDisplayFunc(renderScene);
+	glutMainLoop();
+}
