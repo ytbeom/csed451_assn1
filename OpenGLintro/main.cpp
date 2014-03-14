a51b68ee14830bbe36bc4531e12a3505844483e9
ytbeom <ytbeom@gmail.com>
2014-03-15 01:55:39 +0900

    uh2

--- OpenGLintro/main.cpp
+++ OpenGLintro/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_TRIANGLES);
-	glVertex3f(0.8,0.5,0.0f);
+	glVertex3f(1.0,0.5,0.0f);
 	glVertex3f(0.5,0.0,0.0);
 	glVertex3f(0.0,0.5,0.0);
 	glEnd();
